83b7d0efb51a8683c8d48138569da012bf933058
csutherl <sutherlandcaleb12@gmail.com>
2023-03-02 18:44:32 -0800

    unfinished

---
+++ brevets/flask_brevets.py
@@ -0,0 +1,84 @@
+"""
+Replacement for RUSA ACP brevet time calculator
+(see https://rusa.org/octime_acp.html)
+
+"""
+
+import flask
+from flask import request
+import arrow  # Replacement for datetime, based on moment.js
+import acp_times  # Brevet time calculations
+import config
+from mypymongo import brevet_insert, brevet_find
+
+import logging
+
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+CONFIG = config.configuration()
+
+###
+# Pages
+###
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    app.logger.debug("Main page entry")
+    return flask.render_template('calc.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    return flask.render_template('404.html'), 404
+
+
+###############
+#
+# AJAX request handlers
+#   These return JSON, rather than rendering pages.
+#
+###############
+@app.route("/_calc_times")
+def _calc_times():
+    """
+    Calculates open/close times from miles, using rules
+    described at https://rusa.org/octime_alg.html.
+    Expects one URL-encoded argument, the number of miles.
+    """
+    app.logger.debug("Got a JSON request")
+    km = request.args.get('km', 999, type=float)
+    brevet_dist = request.args.get('brev_dist', 200, type=float)
+    start_time = request.args.get('start_time', type=str)
+    start = arrow.get(start_time, "YYYY-MM-DDTHH:mm")
+    app.logger.debug("km={}".format(km))
+    app.logger.debug("request.args: {}".format(request.args))
+    app.logger.debug("start={}".format(start))
+    # FIXME!
+    # Right now, only the current time is passed as the start time
+    # and control distance is fixed to 200
+    # You should get these from the webpage!
+    open_time = acp_times.open_time(km, brevet_dist, start).format('YYYY-MM-DDTHH:mm')
+    close_time = acp_times.close_time(km, brevet_dist, start).format('YYYY-MM-DDTHH:mm')
+    result = {"open": open_time, "close": close_time}
+    return flask.jsonify(result=result)
+
+
+#@app.route("/_brevet_insert")
+
+#@app.route("/_brevet_fetch")
+
+#############
+
+app.debug = CONFIG.DEBUG
+if app.debug:
+    app.logger.setLevel(logging.DEBUG)
+
+if __name__ == "__main__":
+    print("Opening for global access on port {}".format(CONFIG.PORT))
+    app.run(port=CONFIG.PORT, host="0.0.0.0")
---
+++ brevets/mypymongo.py
@@ -0,0 +1,25 @@
+import os
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
+db = client.mydb
+collection = db.brevets
+
+def brevet_insert(start_time, brevets_dist, checkpoints):
+    output = collection.insert_one({
+        "start_time": start_time,
+        "brevets_dist": brevets_dist,
+        "checkpoints": checkpoints})
+    _id = output.inserted_id
+    return str(_id)
+
+
+def brevet_find():
+    """
+    brevets = collection.find().sort("_id", -1).limit(1)
+
+    for races in brevets:
+       
+        return races["start_time"], races["brevets_dist"], races["checkpoints"]
+     """
+     pass
